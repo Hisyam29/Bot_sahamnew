de224a633693491d183edd1d722311b6565056e3
Hisyam29 <132558542+Hisyam29@users.noreply.github.com>
2026-05-08 09:36:51 +0700

    Update botnew.py

--- botnew.py
+++ botnew.py
@@ -8,7 +8,7 @@ import numpy as np
 # MORA-TYPE SCANNER
 # =========================================================
 
-TOKEN = "8265694791:AAHElCfxfPoB40pZe5yv9tvVcQEIFIAQUAw"
+TOKEN = os.getenv("TOKEN")
 CHAT_IDS = ["1280847575"]
 
 # ==============================
@@ -43,7 +43,7 @@ def send_telegram(message):
 # LOAD SAHAM
 # =========================================================
 def load_symbols():
-    df = pd.read_excel(r"C:\Users\Hisyam\OneDrive\Documents\Coding\saham.xlsx")
+    df = pd.read_excel("saham.xlsx")
 
     print("KOLOM TERDETEKSI:", df.columns)
 
